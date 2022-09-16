6b18f82acd8f232564dfa9b5f31f29f4274374bc
Ilya_Vialiki <Ilya_Vialiki@senla.eu>
2022-09-16 08:41:24 +0200

    01 - Initial commit

---
+++ src/main/java/eu/senla/CollegeLoggerApplication.java
@@ -0,0 +1,13 @@
+package eu.senla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeLoggerApplication.class, args);
+	}
+
+}
