4319b19b6a01f72fdf0ab857be76cfd7d5918558
Ilya_Vialiki <Ilya_Vialiki@senla.eu>
2022-09-21 13:15:35 +0200

    03 - Added and configured ELK-stack

--- src/main/java/eu/senla/kafka/KafkaConsumerService.java
+++ src/main/java/eu/senla/kafka/KafkaConsumerService.java
@@ -1,19 +1,21 @@
 package eu.senla.kafka;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class KafkaConsumerService {
 
     @KafkaListener(topics = "request_logs", groupId = "first")
     public void consumeRequestLog(String requestLog) {
-        System.out.println("REQ LOG: " + requestLog);
+        log.info(requestLog);
     }
 
     @KafkaListener(topics = "token_grant_logs", groupId = "first")
     public void consumeTokenGrantLog(String tokenGrantLog) {
-        System.out.println("TKN LOG: " + tokenGrantLog);
+        log.info(tokenGrantLog);
     }
 
 }
